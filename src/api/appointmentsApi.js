d5680217c6a79aa2c6ae4ba2c0d21d554c436f9b
uttam-marakana <uttammarakana03@gmail.com>
2026-03-03 12:59:51 +0530

    Modified :: Code

--- src/api/appointmentsApi.js
+++ src/api/appointmentsApi.js
@@ -1,16 +1,32 @@
 import {
   collection,
-  getDocs,
   addDoc,
   updateDoc,
   doc,
   serverTimestamp,
+  query,
+  where,
+  getDocs,
+  onSnapshot,
 } from "firebase/firestore";
 import { db } from "../firebase";
 
 const ref = collection(db, "appointments");
 
 export const createAppointment = async (data) => {
+  const q = query(
+    ref,
+    where("date", "==", data.date),
+    where("time", "==", data.time),
+    where("doctorID", "==", data.doctorID),
+  );
+
+  const snap = await getDocs(q);
+
+  if (!snap.empty) {
+    throw new Error("Time slot is Already booked");
+  }
+
   try {
     return await addDoc(ref, {
       ...data,
@@ -23,12 +39,26 @@ export const createAppointment = async (data) => {
   }
 };
 
-export const getAppointments = async () => {
-  const snap = await getDocs(ref);
-  return snap.docs.map((d) => ({ id: d.id, ...d.data() }));
+export const updateAppointmentStatus = async (id, status) => {
+  return await updateDoc(doc(db, "appointments", id), {
+    status,
+    updatedAt: serverTimestamp(),
+  });
 };
 
-export const updateAppointmentStatus = async (id, status) => {
+/* --- Realtime Listener ----------------- */
+export const subscribeAppointment = (callback) => {
+  return onSnapshot(ref, (snap) => {
+    const data = snap.docs.map((d) => ({
+      id: d.id,
+      ...d.data(),
+    }));
+    callback(data);
+  });
+};
+
+/* --- Update Status ----------------- */
+export const getAppointments = async (id, status) => {
   return await updateDoc(doc(db, "appointments", id), {
     status,
     updatedAt: serverTimestamp(),
