1694d30b7bf2d482abb04929f42b56acb7563666
uttam-marakana <uttammarakana03@gmail.com>
2026-03-10 16:10:57 +0530

    Modified :: "pages/" protected subfolder files Code

--- src/pages/protected/Profile.jsx
+++ src/pages/protected/Profile.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth } from "../../firebase";
 import { FaUserCircle, FaEnvelope } from "react-icons/fa";
 
 export default function Profile() {
