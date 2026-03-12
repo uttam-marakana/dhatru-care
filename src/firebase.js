e47520d15b4f5684214e45295f0712e1ec87b0e9
uttam-marakana <uttammarakana03@gmail.com>
2026-03-12 10:38:46 +0530

    Modified :: Firebase file Code

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,12 +14,14 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-
 export const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 
-// SAFE analytics init
+/* SAFE ANALYTICS INIT */
+
 export let analytics = null;
 
 if (typeof window !== "undefined") {
