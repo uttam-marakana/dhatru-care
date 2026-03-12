ed62dc08827f6c4fe931d04f6127ce8c1a914295
uttam-marakana <uttammarakana03@gmail.com>
2026-03-12 14:25:13 +0530

    Modified :: Firebase & Index(Css) files

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+// import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -18,7 +18,7 @@ export const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app);
+// export const storage = getStorage(app);
 
 /* SAFE ANALYTICS INIT */
 
