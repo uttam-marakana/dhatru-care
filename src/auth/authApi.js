94b70cef9255292facf8d7cdd31481c8141cfe8e
uttam-marakana <uttammarakana03@gmail.com>
2026-03-12 10:37:27 +0530

    Modified :: "src/" auth subfolder file

--- src/auth/authApi.js
+++ src/auth/authApi.js
@@ -1,27 +1,19 @@
-import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut,
-} from "firebase/auth";
-
-import { doc, setDoc, serverTimestamp } from "firebase/firestore";
-import { auth, db } from "../firebase";
+import { loginUser, registerUser, logoutUser } from "../services/authService";
+import { createUserProfile } from "../services/userService";
 
 const ADMIN_EMAIL = "uttamrootways@gmail.com";
 
-export const login = (email, password) =>
-  signInWithEmailAndPassword(auth, email, password);
+export const login = (email, password) => loginUser(email, password);
 
 export const signup = async (data) => {
   const { email, password, name, phone, gender, dob, address } = data;
 
-  const res = await createUserWithEmailAndPassword(auth, email, password);
+  const user = await registerUser(email, password);
 
   const role =
     email.toLowerCase() === ADMIN_EMAIL.toLowerCase() ? "admin" : "user";
 
-  await setDoc(doc(db, "users", res.user.uid), {
-    uid: res.user.uid,
+  await createUserProfile(user.uid, {
     name,
     email,
     phone,
@@ -29,10 +21,9 @@ export const signup = async (data) => {
     dob,
     address,
     role,
-    createdAt: serverTimestamp(),
   });
 
-  return res.user;
+  return user;
 };
 
-export const logout = () => signOut(auth);
+export const logout = () => logoutUser();
