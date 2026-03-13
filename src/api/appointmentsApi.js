1dbfa6769fc22447e5ac770cdd60e3d37138542b
uttam-marakana <uttammarakana03@gmail.com>
2026-03-13 16:12:10 +0530

    Modified :: "src/" api subfolder file

--- src/api/appointmentsApi.js
+++ src/api/appointmentsApi.js
@@ -8,24 +8,38 @@ import {
   rescheduleAppointmentService,
 } from "../services/appointmentService";
 
+/* CREATE APPOINTMENT */
+
 export const createAppointment = async (data) => {
   return createAppointmentTransaction(data);
 };
 
+/* ADMIN STATUS UPDATE */
+
 export const updateAppointmentStatus = (id, status) =>
   updateAppointmentStatusService(id, status);
+
+/* CANCEL */
 
 export const cancelAppointment = (appointmentId, slotId) =>
   cancelAppointmentService(appointmentId, slotId);
 
+/* RESCHEDULE */
+
 export const rescheduleAppointment = (appointment, newDate, newTime) =>
   rescheduleAppointmentService(appointment, newDate, newTime);
+
+/* ADMIN APPOINTMENTS */
 
 export const subscribeAppointments = (callback) =>
   subscribeAppointmentsService(callback);
 
+/* USER APPOINTMENTS */
+
 export const subscribeUserAppointments = (userId, callback) =>
   subscribeUserAppointmentsService(userId, callback);
+
+/* DOCTOR SLOT LISTENER */
 
 export const subscribeDoctorSlots = (doctorId, date, callback) =>
   subscribeDoctorSlotsService(doctorId, date, callback);
