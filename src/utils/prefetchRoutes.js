ec6b40400ada3235f1b281483dd23a84620bf860
uttam-marakana <uttammarakana03@gmail.com>
2026-03-14 10:37:39 +0530

    Created :: "src/" utils subfolder file

---
+++ src/utils/prefetchRoutes.js
@@ -0,0 +1,19 @@
+// Prefetch dynamic route chunks
+
+export const routePrefetchMap = {
+  "/departments": () => import("../pages/public/Departments"),
+  "/doctors": () => import("../pages/public/Doctors"),
+  "/packages": () => import("../pages/public/Packages"),
+  "/blog": () => import("../pages/public/Blog"),
+  "/contact": () => import("../pages/public/Contact"),
+  "/login": () => import("../auth/Login"),
+  "/signup": () => import("../auth/Signup"),
+};
+
+export function prefetchRoute(path) {
+  const loader = routePrefetchMap[path];
+
+  if (loader) {
+    loader();
+  }
+}
