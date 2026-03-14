212da6ac5f7211a507b5863ec8978f1dcdd0f37d
uttam-marakana <uttammarakana03@gmail.com>
2026-03-14 12:47:42 +0530

    fix vercel SPA routing + vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
+  base: "/", // ← ADD THIS
+
   plugins: [react(), tailwindcss()],
 
   server: {
@@ -11,7 +9,7 @@ export default defineConfig({
   },
 
   build: {
-    sourcemap: false, // enable only when debugging production
+    sourcemap: false,
     target: "esnext",
     cssCodeSplit: true,
     chunkSizeWarningLimit: 1000,
