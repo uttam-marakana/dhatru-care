27c6f6d95cfe968321f3fce898078744760e0348
uttam-marakana <uttammarakana03@gmail.com>
2026-03-17 15:15:12 +0530

    Modified :: "src/" utils subfolder files

---
+++ src/utils/bookingAnalytics.js
@@ -0,0 +1,32 @@
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
+
+/* TRACK ATTEMPT */
+
+export const trackBookingAttempt = async (data) => {
+  try {
+    await addDoc(collection(db, "bookingEvents"), {
+      type: "attempt",
+      doctorId: data.doctorId,
+      departmentId: data.department,
+      date: data.date,
+      time: data.time,
+      createdAt: serverTimestamp(),
+    });
+  } catch (e) {}
+};
+
+/* TRACK SUCCESS */
+
+export const trackBookingSuccess = async (data) => {
+  try {
+    await addDoc(collection(db, "bookingEvents"), {
+      type: "success",
+      doctorId: data.doctorId,
+      departmentId: data.department,
+      date: data.date,
+      time: data.time,
+      createdAt: serverTimestamp(),
+    });
+  } catch (e) {}
+};
