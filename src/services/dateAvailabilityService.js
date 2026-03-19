b6efb99e740919da3f2be7b26c8da6edaa285cf9
uttam-marakana <uttammarakana03@gmail.com>
2026-03-19 18:28:29 +0530

    Create :: "src/" services subfolder file

---
+++ src/services/dateAvailabilityService.js
@@ -0,0 +1,31 @@
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+
+export const subscribeDoctorDatesAvailability = (doctorId, callback) => {
+  const q = query(
+    collection(db, "appointmentSlots"),
+    where("doctorId", "==", doctorId),
+  );
+
+  return onSnapshot(q, (snap) => {
+    const map = {};
+
+    snap.docs.forEach((d) => {
+      const data = d.data();
+      const date = data.date;
+
+      if (!map[date]) {
+        map[date] = { total: 0, booked: 0 };
+      }
+
+      map[date].total++;
+
+      // 🔥 support both schemas
+      if (data.isBooked || data.booked) {
+        map[date].booked++;
+      }
+    });
+
+    callback(map);
+  });
+};
