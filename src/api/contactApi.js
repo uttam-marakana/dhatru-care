e3ccbd17995f05c28597e7b7c300f863f07efc14
uttam-marakana <uttammarakana03@gmail.com>
2026-03-24 11:44:35 +0530

    Modified :: "src" api folder file

--- src/api/contactApi.js
+++ src/api/contactApi.js
@@ -1,5 +1,20 @@
-import { insertContactMessage } from "../services/contactService";
+import {
+  insertContactMessage,
+  subscribeContactMessages as subscribeService,
+  updateMessageMeta,
+} from "../services/contactService";
 
 /* --- CREATE ----------- */
+export const createContactMessage = async (data) => {
+  return insertContactMessage(data);
+};
 
-export const createContactMessage = (data) => insertContactMessage(data);
+/* --- REALTIME SUBSCRIBE ----------- */
+export const subscribeContacts = (cb) => {
+  return subscribeService(cb);
+};
+
+/* --- UPDATE (STATUS / PRIORITY / NOTES) ----------- */
+export const updateContactMeta = async (id, payload) => {
+  return updateMessageMeta(id, payload);
+};
