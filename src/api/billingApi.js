433b37986e2daeb1cd0c309f08e025a2d5c47a07
uttam-marakana <uttammarakana03@gmail.com>
2026-04-21 13:15:59 +0530

    Created : : "api" Folder files

---
+++ src/api/billingApi.js
@@ -0,0 +1,9 @@
+export { 
+  createInvoice,
+  getInvoices,
+  getInvoiceByAppointmentId,
+  updatePaymentStatus,
+  initiatePayment,
+  getRevenueStats
+} from '../services/billingService'
+
---
+++ src/api/patientApi.js
@@ -0,0 +1,11 @@
+export { 
+  fetchPatients, 
+  fetchPatientById, 
+  searchPatient, 
+  createPatient, 
+  updatePatient, 
+  deletePatient 
+} from '../services/patientService'
+
+export { default as subscribePatients } from '../services/patientService'
+
