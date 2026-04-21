c889f849efc3a30b8a37f9bf4b53bf8a93265332
uttam-marakana <uttammarakana03@gmail.com>
2026-04-21 15:44:52 +0530

    MODIFIED : : "cypress.config.js"  file

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.ts',
+    supportFile: false,
     preprocessor: vitePreprocessor(),
     video: true,
     screenshotOnRunFailure: true,
